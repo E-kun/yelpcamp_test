84699bce1e5dafdbd8593933de383cf544cc3455
E-kun <zentetsukenshi@gmail.com>
2023-08-22 19:52:17 +0800

    first commit

---
+++ seeds/index.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            author: '63c92dca9d7c9a87f470849d',
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://images.unsplash.com/photo-1560931580-af17e379502e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates aliquam assumenda architecto, dolore, iusto officiis aperiam alias voluptas beatae nihil ab accusantium nesciunt itaque esse molestiae ipsum nemo enim sapiente!',
+            price
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
